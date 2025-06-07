bc4da05949d350b00340fe9a33aeb6dc7b94c3bf
Yashu Lilhare <182187926+yashu483@users.noreply.github.com>
2025-06-07 15:02:12 +0530

    feat(html): create initial structure for website

--- using-class.js
+++ using-class.js
@@ -1,21 +1,76 @@
 "use strict";
 
-const myLibrary = [];
+class Book {
 
-class Book{
-    constructor(title, author, pages, status, genre, publishedDate){
-        this.title = title;
-        this.author = author ;
-        this.pages = pages;
-        this.status = status;
-        this.genre = genre;
-        this.publishedDate = publishedDate ;
-        this.id = crypto.randomUUID();
+    #title;
+    #author;
+    #pages;
+    #status;
+    #genre;
+    #publishedDate;
+    #id;
+
+    constructor(title, author, pages, status, genre, publishedDate) {
+        this.#title = title;
+        this.#author = author;
+        this.#pages = pages;
+        this.#status = status;
+        this.#genre = genre;
+        this.#publishedDate = publishedDate;
+        this.#id = crypto.randomUUID();
     };
-};
 
-class addBookToLibrary{
-    constructor(bookObj){
-        myLibrary.push(bookObj);
+    getTitle(){
+        return this.#title;
+    }
+
+    getAuthor (){
+        return this.#author;
+    };
+
+    getPages (){
+        return this.#pages;
+    };
+
+    getStatus (){
+        return this.#status;
+    };
+
+    getGenre(){
+        return this.#genre;
+    };
+
+    getPublishedDate(){
+        return this.#publishedDate;
+    }
+    changeStatus(value) {
+        this.#status = value;
+    };
+
+    getId() {
+        return this.#id;
     }
 };
+
+class LibraryController {
+
+     static   #myLibrary = [];
+
+    static addBookToLibrary(bookObj) {
+        this.#myLibrary.push(bookObj);
+    };
+
+    static removeBook(bookObj) {
+        this.#myLibrary.forEach((obj, index) => {
+            if(bookObj.getId() == obj.getId()){
+                this.#myLibrary.splice(index, 1);
+            }
+        })
+    };
+
+    static  getAllBooks (){
+        return this.#myLibrary;
+    }
+};
+
+
